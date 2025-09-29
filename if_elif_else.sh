331d7354ee66fb85cdf0d16c9bf7c9132c3d4fe5
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-29 23:28:56 +0530

    conditions

--- if_elif_else.sh
+++ if_elif_else.sh
@@ -12,7 +12,7 @@ else
 fi
 #even or odd numbers
 
-if [ $NUMBER % 2 -eq 0 ]; then
+if [ "$NUMBER % 2" -eq 0 ]; then
     echo "given number $NUMBER is even"
 else 
     echo "given number $NUMBER is odd"
