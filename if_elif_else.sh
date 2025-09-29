4744f6b57b063d641d5e60fa7ccaac1dedaa9473
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-29 22:44:45 +0530

    conditions

---
+++ if_elif_else.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#print which number is greater and less and equal to 10
+#shell script is depending on commonsense
+
+NUMBER=$1
+if [ NUMBER -lt 10 ]; then
+    echo "given number $1 lessthan 10"
+elif [ NUMBER -eq 10 ]; then
+    echo "given number is 10"
+else 
+    echo "given number $1 is greaterthan 10"
