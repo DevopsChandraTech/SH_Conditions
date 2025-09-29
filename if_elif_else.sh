3b7ac96a50cf8b35c1ec86e774a53fb67a2229f6
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-29 22:48:24 +0530

    conditions

--- if_elif_else.sh
+++ if_elif_else.sh
@@ -7,6 +7,6 @@ if [ NUMBER -lt 10 ]; then
     echo "given number $1 lessthan 10"
 elif [ NUMBER -eq 10 ]; then
     echo "given number is 10"
-else;
+else
     echo "given number $1 is greaterthan 10"
 fi
