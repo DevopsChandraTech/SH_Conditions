5092da5156d4942c48c667a3ccb85e0f8eaa39a3
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-30 22:32:58 +0530

    conditions

--- install_mysql.sh
+++ install_mysql.sh
@@ -3,12 +3,12 @@ USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ];then
     echo "Error:: user has not root priviliges"
-    exit 1
+    exit 1 # if script fails then exit here not proceed further
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ];then
     echo "Error:: fail the mysql installation"
-    exit 1
+    exit 1 # if script fails then exit here not proceed further
 fi
