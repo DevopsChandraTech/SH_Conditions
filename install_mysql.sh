68dd6da9a7e8047167fe74e0e10fd010997f1cae
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-30 23:17:38 +0530

    conditions

--- install_mysql.sh
+++ install_mysql.sh
@@ -6,9 +6,26 @@ if [ $USER_ID -ne 0 ];then
     exit 1 # if script fails then exit here not proceed further
 fi
 
+#mysql installation:
 dnf install mysql -y
 
 if [ $? -ne 0 ];then
     echo "Error:: fail the mysql installation"
+    exit 1 # if script fails then exit here not proceed further
+fi
+
+#nginx installation:
+dnf install nginx -y
+
+if [ $? -ne 0 ];then
+    echo "Error:: fail the nginx installation"
+    exit 1 # if script fails then exit here not proceed further
+fi
+
+#python3 installation:
+dnf install python3 -y
+
+if [ $? -ne 0 ];then
+    echo "Error:: fail the python3 installation"
     exit 1 # if script fails then exit here not proceed further
 fi
