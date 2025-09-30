f877654020e5d489d670c5551f36094c49e4edc0
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-30 20:11:22 +0530

    conditions

---
+++ install_mysql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USER=$(id -u)
+if [ $USER -nq 0 ];then
+    echo "Error:: user has not root priviliges"
+else
+    echo "pls install the application"
+dnf install -y
+if [ $? -eq 0 ];then
+    echo "Success:: the installation"
+else
+    echo "Error:: not installing the app"
+fi
