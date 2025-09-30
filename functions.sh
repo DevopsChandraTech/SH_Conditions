fff3fe5b90a6fcc9f517dab47fb32f40d2aa98df
DevOpsChandraTech <devopschandra42@gmail.com>
2025-09-30 23:40:14 +0530

    conditions

--- functions.sh
+++ functions.sh
@@ -5,27 +5,20 @@ if [ $USER_ID -ne 0 ]; then
     echo "ERROR:: user have not root privilizes"
     exit 1 # script exit if there error above
 fi
-
+VALIDATE(){ #functions taken inputs as a args like shell script args
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: fail the installaton"
+        exit 1 # if fails the installation exit script here
+    else
+        echo "Success:: the $2 installation"
+    fi
+}
 #mysql installation:
 dnf install mysql -y
-
-if [ $? -ne 0 ]; then
-    echo "ERROR:: fail the installaton"
-    exit 1 # if fails the installation exit script here
-fi
-
+VALIDATE $? mysql
 #nginx installation:
 dnf install nginx -y
-
-if [ $? -ne 0 ]; then
-    echo "ERROR:: fail the installaton"
-    exit 1 # if fails the installation exit script here
-fi
-
+VALIDATE $? nginx
 #unzip installation:
 dnf install unzip -y
-
-if [ $? -ne 0 ]; then
-    echo "ERROR:: fail the installaton"
-    exit 1 # if fails the installation exit script here
-fi
+VALIDATE $? unzip
