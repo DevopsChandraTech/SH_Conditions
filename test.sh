5586935c824f4037ff36eb0b6ecf0b91f7dae603
DevOpsChandraTech <devopschandra42@gmail.com>
2025-10-01 22:14:52 +0530

    conditions

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR:: run script with root user privilizes"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    echo "server not installed into this system"
+    dnf install mysql -y
+else    
+    echo "mysql already exists skipping...!"
+
