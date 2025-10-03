b96f061a1cb678933760db88f52b201df9d0376f
DevOpsChandraTech <devopschandra42@gmail.com>
2025-10-03 22:54:03 +0530

    Conditions

--- practice.sh
+++ practice.sh
@@ -3,12 +3,14 @@
 #id -u
 USER_ID=$(id -u)
 
-if [$USER_ID -ne 0 ]; then
+if [ $USER_ID -ne 0 ]; then
     echo "ERROR:: NEED TO RUN WITH ROOT USER PRIVILIZES"
+    exit 1
 fi
 dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "ERROR:: COMMAND NOT FOUND PLS CHECK ONCE THE COMMAND"
+    exit 1
 else 
     echo "SUCCESS:: MYSQL INSTALLATION IS SUCCESSFULL"
 fi
