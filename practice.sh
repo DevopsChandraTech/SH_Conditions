bb44ba14a081f6d607f3c6bce2a54882091d40ce
DevOpsChandraTech <devopschandra42@gmail.com>
2025-10-03 22:52:06 +0530

    conditions

--- practice.sh
+++ practice.sh
@@ -1,56 +1,14 @@
 #!/bin/bash
-# installing the sql nginx unzip python3 using script
-
+#check the user has root privilizes or not
+#id -u
 USER_ID=$(id -u)
 
-if [ $USER_ID -ne 0 ]; then
-    echo "ERROR:: installed user has no root privilizes"
-    exit 1
+if [$USER_ID -ne 0 ]; then
+    echo "ERROR:: NEED TO RUN WITH ROOT USER PRIVILIZES"
 fi
-
-VALIDATE(){ #functions take inputs as args like shell script args
-
-if [ $1 -ne 0 ]; then
-    echo "ERROR:: the command not found"
-    exit 1
-else
-    echo -e "Success:: the $2 installation \e[32m Successful \e[33m"
-fi
-}
-
-#mysql installation:
-dnf list installed mysql 
+dnf install mysql -y
 if [ $? -ne 0 ]; then
-    echo -e "ERROR:: $2 not installed into this system \e[31m"
-    dnf install mysql -y
-    VALIDATE $? mysql
-else
-    echo -e "\e[32m SUCCESS \e[0m :: $2 already installed \e[33m SKIPPING \e[0m"
-fi
-#nginx installation:
-dnf list installed nginx
-if [ $? -ne 0 ]; then
-    echo -e "ERROR:: $2 not installed into this system \e[31m Failure \e[0m"
-    dnf install nginx -y
-    VALIDATE $? nginx
-else
-    echo -e "\e[32m SUCCESS \e[0m :: $2 already installed \e[33m SKIPPING \e[0m"
-fi
-#unzip installation:
-dnf list installed unzip
-if [ $? -ne 0 ]; then
-    echo -e "ERROR:: $2 not installed into this system \e[31m Failure \e[0m"
-    dnf install unzip -y
-    VALIDATE $? unzip
-else
-    echo -e "\e[32m SUCCESS \e[0m :: $2 already installed \e[33m SKIPPING \e[0m"
-fi
-#unzip installation:
-dnf list installed python3
-if [ $? -ne 0 ]; then
-    echo -e "ERROR:: $2 not installed into this system \e[31m Failure \e[0m"
-    dnf install python3 -y
-    VALIDATE $? python3
-else
-    echo -e "\e[32m SUCCESS \e[0m :: $2 already installed \e[33m SKIPPING \e[0m"
+    echo "ERROR:: COMMAND NOT FOUND PLS CHECK ONCE THE COMMAND"
+else 
+    echo "SUCCESS:: MYSQL INSTALLATION IS SUCCESSFULL"
 fi
