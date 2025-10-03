9ac90e3cbdbaaad6aa3ac2bf76cb7aca32374ba3
DevOpsChandraTech <devopschandra42@gmail.com>
2025-10-03 22:57:55 +0530

    Conditions

--- practice.sh
+++ practice.sh
@@ -7,10 +7,19 @@ if [ $USER_ID -ne 0 ]; then
     echo "ERROR:: NEED TO RUN WITH ROOT USER PRIVILIZES"
     exit 1
 fi
+#mysql installation
 dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "ERROR:: COMMAND NOT FOUND PLS CHECK ONCE THE COMMAND"
     exit 1
 else 
     echo "SUCCESS:: MYSQL INSTALLATION IS SUCCESSFULL"
+fi
+#unzip installation
+dnf installl unzip -y
+if [ $? -ne 0 ]; then
+    echo "ERROR:: COMMAND NOT FOUND PLS CHECK ONCE THE COMMAND"
+    exit 1
+else 
+    echo "SUCCESS:: UNZIP INSTALLATION IS SUCCESSFULL"
 fi
