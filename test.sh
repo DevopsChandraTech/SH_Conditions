687dc6edfbb232726372639798498016deee013f
DevOpsChandraTech <devopschandra42@gmail.com>
2025-10-06 15:50:33 +0530

    conditions

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILENAME="$LOG_FOLDER/$SCRIPT_NAME.log"
+LOG_FILENAME="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 
 if [ $USER_ID -ne 0 ]; then
@@ -30,25 +30,25 @@ if [ $? -ne 0 ]; then
     dnf install unzip -y &>>$LOG_FILENAME
     VALIDATE $? UNZIP
 else    
-    echo -e "$2 already installed $Y Skipping $N ...!" &>>LOG_FILENAME
+    echo -e "$2 already installed $Y Skipping $N ...!" &>>$LOG_FILENAME
 fi
 #tree installation
 dnf list installed tree -y &>>$LOG_FILENAME
 if [ $? -ne 0 ]; then
-    echo -e "$2 NOT INSTALLED::install the $2 on this system" &>>LOG_FILENAME
+    echo -e "$2 NOT INSTALLED::install the $2 on this system" &>>$LOG_FILENAME
     dnf install tree -y &>>$LOG_FILENAME
     VALIDATE $? TREE
 else    
-    echo -e "$2 already installed $Y Skipping $N ...!" &>>LOG_FILENAME
+    echo -e "$2 already installed $Y Skipping $N ...!" &>>$LOG_FILENAME
 fi
 #nginx installation
 dnf list installed nginx -y &>>$LOG_FILENAME
 if [ $? -ne 0 ]; then
-    echo -e "NOT INSTALLED::install the $2 on this system" &>>LOG_FILENAME
+    echo -e "NOT INSTALLED::install the $2 on this system" &>>$LOG_FILENAME
     dnf install nginx -y &>>$LOG_FILENAME
     VALIDATE $? NGINX
 else    
-    echo -e "$2 already installed $Y Skipping $N ...!" &>>LOG_FILENAME
+    echo -e "$2 already installed $Y Skipping $N ...!" &>>$LOG_FILENAME
 fi
 
 
