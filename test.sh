bf1d5a69c2626157c89012dcabe436744c318917
DevOpsChandraTech <devopschandra42@gmail.com>
2025-10-06 14:02:57 +0530

    conditions

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ $USER_ID -ne 0 ]; then
     echo "Error::User has not root privilizes"
     exit 1
@@ -16,11 +21,11 @@ fi
 #unzip installation
 dnf list installed unzip -y
 if [ $? -ne 0 ]; then
-    echo "NOT INSTALLED::install the $2 on this system"
+    echo "$R NOT INSTALLED $N ::install the $2 on this system"
     dnf install unzip -y
     VALIDATE $? MYSQL
 else    
-    echo "$2 already installed Skipping...!"
+    echo -e "$2 already installed $Y Skipping $N ...!"
 fi
 #tree installation
 dnf list installed tree -y
@@ -29,7 +34,7 @@ if [ $? -ne 0 ]; then
     dnf install tree -y
     VALIDATE $? TREE
 else    
-    echo "$2 already installed Skipping...!"
+    echo "$2 already installed $Y Skipping $N ...!"
 fi
 #nginx installation
 dnf list installed nginx -y
@@ -38,7 +43,7 @@ if [ $? -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? NGINX
 else    
-    echo "$2 already installed Skipping...!"
+    echo "$2 already installed $Y Skipping $N ...!"
 fi
 
 
